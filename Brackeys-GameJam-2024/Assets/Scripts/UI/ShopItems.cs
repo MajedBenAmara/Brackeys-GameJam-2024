3ecf2ec6e68d7202d42e6ec3ae0ae623c0b928bd
Shri Hari <shriharishanmugam@gmail.com>
2024-09-10 23:48:35 +0530

    Reapply "Prototype of Shop Screen"
    This reverts commit 974d73781e9582cc432b1591f054e30e7ea9dcd1.

---
+++ Brackeys-GameJam-2024/Assets/Scripts/UI/ShopItems.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItems : MonoBehaviour
+{
+    [SerializeField] Item[] List;
+
+    [SerializeField] Image ItemSprite;
+    [SerializeField] Text InfoBox;
+    [SerializeField] Text Price;
+    [SerializeField] Button Buy;
+
+    private void Start()
+    {
+        foreach (Item item in List)
+        {
+            Button button = item.gameObject.GetComponent<Button>();
+            button.onClick.AddListener(() => UpdateScreen(item));
+        }
+        if(List.Length > 0)
+        {
+
+            UpdateScreen(List[0]);
+        }
+    }
+
+    private void UpdateScreen(Item referance)
+    {
+        ItemSprite.sprite = referance.Sprite;
+        InfoBox.text = $"Name: {referance.Name}\nDescription: {referance.Description}";
+        Price.text = $"Price {referance.Price}G";
+        Buy.onClick.RemoveAllListeners();
+        Buy.onClick.AddListener(referance.Buy);
+    }
+
+}
